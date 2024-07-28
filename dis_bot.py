e15d93913e03fc895cc581d13b318544907acf6b
hiedhkj <138582274+hiedhkj@users.noreply.github.com>
2024-07-28 13:53:16 +0330

    درست کردن سورس

--- dis_bot.py
+++ dis_bot.py
@@ -5,18 +5,14 @@ intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='~', intents=intents)
 intents.messages = True
 
-@bot.tree.command(name="ping" , description="Check the bot speed" , guild = discord.Object(id=1266707957467451482))
-async def slash_command(interaction: discord.Interaction):
-
-    await interaction.response.send_message("Pong!")
-
-
-dghdghrfdrg
-hdfgbhjdtgkjdfjih
-ghfghghgdh
 @bot.event
 async def on_ready():
     await bot.tree.sync(guild=discord.Object(id=1266707957467451482))
     print(f'{bot.user}')
+
+@bot.tree.command(name="ping" , description="Check the bot speed" , guild = discord.Object(id=1266707957467451482))
+async def slash_command(interaction: discord.Interaction):
+
+    await interaction.response.send_message("Pong!")
 
 bot.run('')
