2b1bb9fe497433c386e9fb1344bd6cd7515bf91e
sirod <amiralisarshar12@gmail.com>
2024-07-28 12:48:07 +0330

    help added

--- dis_bot.py
+++ dis_bot.py
@@ -1,3 +1,18 @@
-print('kos khar ali.h1')
-kos khar ali.h فلف
-ب
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='~', intents=intents)
+intents.messages = True
+
+@bot.tree.command(name="ping" , description="Check the bot speed" , guild = discord.Object(id=1266707957467451482))
+async def slash_command(interaction: discord.Interaction):
+
+    await interaction.response.send_message("Pong!")
+
+@bot.event
+async def on_ready():
+    await bot.tree.sync(guild=discord.Object(id=1266707957467451482))
+    print(f'{bot.user}')
+
+bot.run('')
