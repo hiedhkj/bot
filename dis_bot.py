3359c98cce49fdd57b09405cec62c327f9717989
sirod <amiralisarshar12@gmail.com>
2024-07-29 14:28:37 +0330

    Comments Added (منظم تر شد)

--- dis_bot.py
+++ dis_bot.py
@@ -1,14 +1,22 @@
+#===Imports===>
+
 import discord
 from discord.ext import commands
+
+#===نمیدونم اینجا چیه===>
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='~', intents=intents)
 intents.messages = True
 
+#===Login===>
+
 @bot.event
 async def on_ready():
     await bot.tree.sync(guild=discord.Object(id=1266707957467451482))
     print(f'{bot.user}')
+
+#===Commands===>
 
 @bot.tree.command(name="ping" , description="Check the bot speed" , guild = discord.Object(id=1266707957467451482))
 async def slash_command(interaction: discord.Interaction):
