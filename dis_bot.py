b564a7f94e88c4bbc1dd6f32d3218c638b6f59ec
sirod <amiralisarshar12@gmail.com>
2024-07-29 14:41:04 +0330

    Kick command added

--- dis_bot.py
+++ dis_bot.py
@@ -22,4 +22,19 @@ async def on_ready():
 async def slash_command(interaction: discord.Interaction):
     await interaction.response.send_message(f"Pong! Latency: {round(bot.latency * 1000)} ms")# پینگ باتو روند میکنه اعشاری نشون نمیده
 
+@bot.tree.command(name="kick" , description="Kick a Member." , guild = discord.Object(id=1266707957467451482))
+async def kick(interaction: discord.Interaction , member : discord.member):
+    if interaction.author.guild_permissions.kick_members:
+        if member.top_role < interaction.author.top_role:
+            if member:
+                await member.kick(reason="You have been banned.")
+                embed = discord.Embed(title=":white_check_mark: Banned!", description=f"{member.name} Kicked by {interaction.author}")
+                await interaction.response.send_message(embed=embed)
+            else:
+                await interaction.response.send_message('error')
+        else:
+            await interaction.response.send_message(f"You Can't Kick {member.name}")
+    else:
+        await interaction.response.send_message("You Cant' Kick Members")
+
 bot.run('')
