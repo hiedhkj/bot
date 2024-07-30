d132d83d78e319779f644e24668594eefb13e216
sirod <amiralisarshar12@gmail.com>
2024-07-30 16:26:41 +0330

    clear cmd fixed and quiz deleted

--- dis_bot.py
+++ dis_bot.py
@@ -49,12 +49,16 @@ async def avatar(interaction: discord.Interaction , member: discord.Member):
 #-=-Clear-=->
 @bot.tree.command(name="clear" , description="Clear Messages." , guild = g)
 async def clear(interaction: discord.Interaction , number_of_message: int):
-    if number_of_message == None:
-        await interaction.channel.purge(limit=3)
+    if interaction.user.guild_permissions.manage_messages:
+        if number_of_message == None:
+            await interaction.channel.purge(limit=3)
+        else:
+            embed = discord.Embed(title=f':white_check_mark: Clearing {number_of_message} Message' , color = discord.Color.green())
+            await interaction.response.send_message(embed = embed , ephemeral = True)
+            await interaction.channel.purge(limit=number_of_message)
     else:
-        embed = discord.Embed(title=f':white_check_mark: Clearing {number_of_message} Message' , color = discord.Color.green())
+        embed = discord.Embed(title=":x: You Can't Clear Messages.")
         await interaction.response.send_message(embed = embed)
-        await interaction.channel.purge(limit=number_of_message)
 #-=-Kick-=->
 @bot.tree.command(name="kick" , description="Kick a Member." , guild = g)
 async def kick(interaction: discord.Interaction , member : discord.Member):
@@ -106,84 +110,6 @@ async def ban(interaction: discord.Interaction , id : str):
             await interaction.response.send_message("❌ Member not found in the ban list.")
     else:
         await interaction.response.send_message("❌ You do not have permission")
-
-#===Quis===>
-
-@bot.tree.command(name="quiz" , description="Start Quiz Game." , guild = g)
-async def quiz(interaction: discord.Interaction):
-
-    btn1 = Button(label="1" , style=discord.ButtonStyle.gray)
-    btn2 = Button(label="2" , style=discord.ButtonStyle.red)
-    btn3 = Button(label="3" , style=discord.ButtonStyle.green)
-    btn4 = Button(label="4" , style=discord.ButtonStyle.blurple)
-
-    soalat = [
-        "https://cdn.discordapp.com/attachments/1266707957467451485/1267492952674930719/soal2.png?ex=66a8fc4c&is=66a7aacc&hm=34474047074465375357deadf21e50a3e6c157810ec3b0e82cb2b6d9b05ba5b6&",
-        "https://cdn.discordapp.com/attachments/1266707957467451485/1267492952343707658/soal1.png?ex=66a8fc4c&is=66a7aacc&hm=8de60524789c9de9b88cc3793cfea53b85428cc1e99797e5beade56c75ff1966&"
-    ]
-
-    b = random.choice(soalat)
-
-    async def btn1_callback(interaction):
-        embed = discord.Embed(title="❌ Wrong!" , color=discord.Color.red())
-        await interaction.response.send_message(embed = embed)
-        btn1.disabled = True
-        btn2.disabled = True
-        btn3.disabled = True
-        btn4.disabled = True
-
-    async def btn2_callback(interaction):
-        if b == "https://cdn.discordapp.com/attachments/1266707957467451485/1267492952343707658/soal1.png?ex=66a8fc4c&is=66a7aacc&hm=8de60524789c9de9b88cc3793cfea53b85428cc1e99797e5beade56c75ff1966&":
-            embed = discord.Embed(title=":white_check_mark: Correct!" , color=discord.Color.green())
-            await interaction.response.send_message(embed = embed)
-            btn1.disabled = True
-            btn2.disabled = True
-            btn3.disabled = True
-            btn4.disabled = True
-        else:
-            embed = discord.Embed(title="❌ Wrong!" , color=discord.Color.red())
-            await interaction.response.send_message(embed = embed)
-            btn1.disabled = True
-            btn2.disabled = True
-            btn3.disabled = True
-            btn4.disabled = True
-
-    async def btn3_callback(interaction):
-        if b == "https://cdn.discordapp.com/attachments/1266707957467451485/1267492952674930719/soal2.png?ex=66a8fc4c&is=66a7aacc&hm=34474047074465375357deadf21e50a3e6c157810ec3b0e82cb2b6d9b05ba5b6&":
-            embed = discord.Embed(title=":white_check_mark: Correct!" , color=discord.Color.green())
-            await interaction.response.send_message(embed = embed)
-            btn1.disabled = True
-            btn2.disabled = True
-            btn3.disabled = True
-            btn4.disabled = True
-        else:
-            embed = discord.Embed(title="❌ Wrong!" , color=discord.Color.red())
-            await interaction.response.send_message(embed = embed)
-            btn1.disabled = True
-            btn2.disabled = True
-            btn3.disabled = True
-            btn4.disabled = True
-
-    async def btn4_callback(interaction):
-        embed = discord.Embed(title="❌ Wrong!" , color=discord.Color.red())
-        await interaction.response.send_message(embed = embed)
-        btn1.disabled = True
-        btn2.disabled = True
-        btn3.disabled = True
-        btn4.disabled = True
-
-    btn1.callback = btn1_callback
-    btn2.callback = btn2_callback
-    btn3.callback = btn3_callback
-    btn4.callback = btn4_callback
-
-    view = View()
-    view.add_item(btn1)
-    view.add_item(btn2)
-    view.add_item(btn3)
-    view.add_item(btn4)
-
-    await interaction.response.send_message(b , view = view)
 
 #===Run===>
 bot.run('')
